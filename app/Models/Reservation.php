36d7d65d5db44bacc5e3cdf1b607137a2153b170
AmelyaSofia <amelyasofia18@gmail.com>
2026-06-17 11:51:24 +0700

    soft delete

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -48,7 +48,7 @@ class Reservation extends Model
      */
     public function room(): BelongsTo
     {
-        return $this->belongsTo(Room::class);
+        return $this->belongsTo(Room::class)->withTrashed();
     }
 
     // ─── Helpers ───────────────────────────────────────────────────────────
